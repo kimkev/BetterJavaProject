a44fc69d0532033700f4b6eb5df2246d44cb7b93
Kevin Kim <kimkevi@sheridancollege.ca>
2019-04-07 18:51:48 -0400

    added lombok

--- JavaProject/src/main/java/ca/sheridancollege/ca/JavaProject/Person.java
+++ JavaProject/src/main/java/ca/sheridancollege/ca/JavaProject/Person.java
@@ -5,17 +5,26 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
+@Data
+@AllArgsConstructor
 @Entity
 public class Person {
 
-		@Id
-		@GeneratedValue
-		private Long id;
-		
-		@Column(name = "Name", length = 64, nullable = false)
-		private String name;
-	
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@Column(name = "Name", length = 64, nullable = false)
+	private String name;
+
+	public Person(String name) {
+		super();
+		this.name = name;
+	}
+
 }
